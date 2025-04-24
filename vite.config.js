39c755317c2ba071294229386455acbe3e4511de
Sylviaviaviavia <103002698+Sylviaviaviavia@users.noreply.github.com>
2025-04-23 20:29:04 -0400

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/paper-recommendation/', 
   plugins: [react()],
   server: {
     proxy: {
@@ -14,3 +15,4 @@ export default defineConfig({
     },
   },
 })
+
